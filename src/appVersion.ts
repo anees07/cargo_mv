48b1f6ad0e9634a4bab65e54e3b921cdc64b61e6
anees07 <mohamedanys@gmail.com>
2026-06-14 13:14:15 +0500

    final Icons uodates

--- src/appVersion.ts
+++ src/appVersion.ts
@@ -1,4 +1,4 @@
-export const APP_VERSION = "1.0.2";
-export const APP_BUILD = "3";
+export const APP_VERSION = "1.0.3";
+export const APP_BUILD = "4";
 export const APP_RELEASE_LABEL = `v${APP_VERSION} (${APP_BUILD})`;
 export const APP_RELEASE_DETAIL = `Version ${APP_VERSION} • Build ${APP_BUILD}`;
